86fa51314c0079faceb00225f3bdad5f5ab9faef
Luciane Souza <luciane.sz@hotmail.com>
2020-03-10 10:19:22 -0300

    exercicio 1 resolvido

---
+++ tarefa1.php
@@ -0,0 +1,83 @@
+<?php
+print "a. Uma variável com um número inteiro";
+echo "<br>";
+
+$idade = 17;
+var_dump($idade);
+
+echo "<hr>";
+
+//------------------------------------------
+
+print "b. Uma variável com um número decimal.";
+echo "<br>";
+
+$altura = 1.68;
+var_dump($altura);
+
+echo "<hr>";
+
+//------------------------------------------
+
+print "c. Uma variável com um caractere, usando aspas duplas.";
+echo "<br>";
+
+$letra = "A";
+var_dump($letra);
+
+echo "<hr>";
+
+//------------------------------------------
+print "d. Uma variável com um caractere, usando aspas simples.";
+echo "<br>";
+
+$letra1 = 'A';
+var_dump($letra1);
+
+echo "<hr>";
+//------------------------------------------
+print " e. Uma variável com string usando aspas simples.";
+echo "<br>";
+
+$nome = 'Luciane';
+var_dump($nome);
+
+echo "<hr>";
+//-----------------------------------------
+print "f. Uma variável com string usando aspas duplas";
+echo "<br>";
+
+$nome = "Luciane";
+var_dump($nome);
+
+echo "<hr>";
+//------------------------------------------
+
+print "g. Atribuir uma string à variável do ponto a";
+echo "<br>";
+
+$idade =  "17";
+var_dump($idade);
+
+echo "<hr>";
+//------------------------------------------
+print " h. Atribuir um número decimal à variável do ponto e.";
+echo "<br>";
+
+$nome = 1.68;
+var_dump($nome);
+
+echo "<hr>";
+//------------------------------------------
+
+print "i. O que acontece com o tipo de dado das variáveis do ponto g e do ponto h?";
+echo "<br>";
+print  " R: O tipo de dados mudou de inteiro para string e de string para float.";
+echo "<hr>";
+//------------------------------------------
+print " j. O que acontece se atribuirmos outro valor numérico à variável do ponto a,
+mas entre aspas?";
+echo "<br>";
+print  " R: O número será interpretado com uma string.";
+
+
