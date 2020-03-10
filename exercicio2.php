af098ff6bc51283b44bc068a13b682a247b11b5f
Luciane Souza <luciane.sz@hotmail.com>
2020-03-10 18:42:05 -0300

    PHP introdução tarefa 1 exercicio 2

---
+++ exercicio2.php
@@ -0,0 +1,19 @@
+<?php
+
+$animais = array();
+$animais[]="cachorro";
+$animais[]="gato";
+$animais[]="tartaruga";
+$animais[]="passarinho";
+$animais[]="rato";
+$animais[]="galinha";
+$animais[]= "galo";
+$animais[0]="ovelha";
+$animais[100]="cavalo";
+$animais[16]="porco";
+
+var_dump($animais);
+print_r($animais);
+
+echo "Eu gosto de animais: $animais[0], $animais[1], $animais[2], $animais[3], $animais[4], $animais[5] e $animais[6]";
+
