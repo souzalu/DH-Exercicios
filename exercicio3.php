3ebe67fd7d7acc09f79fec9d2087f4e068e6f03c
Luciane Souza <luciane.sz@hotmail.com>
2020-03-14 18:02:31 -0300

    Exercicio 3 tarefa 1 PHP introdução

---
+++ exercicio3.php
@@ -0,0 +1,12 @@
+<?php
+
+$carro = array("Marca"=>"Uno", "Modelo"=>"Vivace", "Cor"=>"Azul", "Ano"=>"2020", "Placa"=>"XCD4758");
+$carro[0]="Rodrigo";
+$carro[14]="Porto Seguro";
+$carro["Apólice"]="123456";
+$carro[0]="Sergio";
+$carro["Placa"]="BVC 4141";
+
+print_r($carro);
+var_dump($carro);
+
